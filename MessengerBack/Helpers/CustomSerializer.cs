5b95c0ddba6ea5b7b412257552959cad8a4477e9
Maksym Lukashevych <orki4651@gmail.com>
2023-06-21 23:32:45 +0300

    # hw10

--- MessengerBack/Helpers/CustomSerializer.cs
+++ MessengerBack/Helpers/CustomSerializer.cs
@@ -12,7 +12,19 @@ public static class CustomSerializer
         var props = type.GetProperties();
         foreach (var prop in props)
         {
-            result.Append($"{prop.Name}:{prop.GetValue(obj)};");
+            // --- Можно ли это написать красивее? 
+            var attribute = Attribute
+                .GetCustomAttribute(prop, typeof(CustomSerializerAttribute)) 
+                as CustomSerializerAttribute;
+            if (attribute is not null)
+            {
+                result.Append($"{attribute.CustomName ?? prop.Name}:{prop.GetValue(obj)};");
+            }
+            else
+            {
+                result.Append($"{prop.Name}:{prop.GetValue(obj)};");
+            }
+            // ---
         }
 
         return result.ToString();
@@ -21,22 +33,60 @@ public static class CustomSerializer
     {
         var result = Activator.CreateInstance(type);
         var resultType = result.GetType();
-        var regex = new Regex("([a-z0-9A-Z]+):([a-z0-9A-Z]+)");
-        var props = str.Split(';');
-        foreach (var prop in props)
+        var regex = new Regex(@"(\w+):(\w+)");         // (\w+):(\w+) ([a-z0-9A-Z]+):([a-z0-9A-Z]+)
+        var matches = regex.Matches(str);
+        //var props = str.Split(';');
+
+        foreach (var prop in resultType.GetProperties())
         {
-            if (!String.IsNullOrWhiteSpace(prop))
-            {   
-                var mach = regex.Match(prop);
-                resultType.GetProperty(mach.Groups[1].Value)
-                    .SetValue(result, mach.Groups[2].Value);
+            var attr = prop.GetCustomAttribute<CustomSerializerAttribute>();
+            if (attr is not null)
+            {
+                var propVal = matches
+                    .Select(x => x.Groups).FirstOrDefault(x => x[1].Value == attr.CustomName);
+                if (propVal is not null)
+                {
+                    prop.SetValue(result, propVal[2].Value);
+                }
+                else
+                {
+                    propVal = matches
+                        .Select(x => x.Groups).FirstOrDefault(x => x[1].Value == prop.Name);
+                    prop.SetValue(result, propVal[2].Value);
+                }
             }
+            else
+            {
+                var propVal = matches
+                    .Select(x => x.Groups).FirstOrDefault(x => x[1].Value == prop.Name);
+                prop.SetValue(result, propVal[2].Value);
+            }
+            
         }
+        
+        // foreach (var prop in props)
+        // {
+        //     if (!String.IsNullOrWhiteSpace(prop))
+        //     {   
+        //         var mach = regex.Match(prop);
+        //         resultType.GetProperty(mach.Groups[1].Value)
+        //             .SetValue(result, mach.Groups[2].Value);
+        //     }
+        // }
         return result;
     }
 
     public static T DeserializeObj<T>(string str)
     {
         return (T)DeserializeObj(str, typeof(T));
+    }
+}
+
+public class CustomSerializerAttribute : Attribute
+{
+    public string? CustomName { get; set; }
+    public CustomSerializerAttribute(string? name)
+    {
+        CustomName = name;
     }
 }
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,5 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.ComponentModel;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using MessengerBack.Entities;
 using MessengerBack.Helpers;
 
@@ -14,8 +17,11 @@ string userStr = CustomSerializer.SerializeObj(user, typeof(User));
 User newUser = (User)CustomSerializer.DeserializeObj(userStr, typeof(User));
 User newUser2 = CustomSerializer.DeserializeObj<User>(userStr);
 Console.WriteLine(userStr);
+JsonSerializer.Serialize(user);
+var srt = JsonSerializer.Serialize(user);
 class User
 {
+    [CustomSerializer("null")]
     public string Name { get; set; }
     public string LastName { get; set; }
 }
